30fbcbd83e482785909c9b19cbd789db3f8f7b9a
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-09 21:18:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each{|student| Student.new
+    students_array.each{|student| Student.new(student)}
     
   end
 
