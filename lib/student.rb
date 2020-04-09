6334b77a146519ad617182762459e29e06beedcc
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-09 21:19:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,14 +13,19 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each{|student| Student.new(student)}
-    
   end
 
   def add_student_attributes(attributes_hash)
-    
+    self.send("twitter=", attributes_hash[:twitter])
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("github=", attributes_hash[:github])
+    self.send("blog=", attributes_hash[:blog])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
+    self.send("bio=", attributes_hash[:bio])
   end
 
   def self.all
     @@all
   end
+  
 end
